5c9f4184ccf7e2e666e4a95bec1b405bd70bbef4
Aron Strandberg <strandberg.aron@gmail.com>
2017-07-02 16:33:46 +0200

    Add fish to /etc/shells before switching to it

--- fish/install.sh
+++ fish/install.sh
@@ -1,5 +1,8 @@
 # set fish as default shell
-chsh -s /usr/bin/fish
+if ! fgrep -q '/usr/local/bin/fish' /etc/shells; then
+  echo '/usr/local/bin/fish' | sudo tee -a /etc/shells
+fi
+chsh -s /usr/local/bin/fish
 
 # install oh-my-fish
 curl -L https://github.com/oh-my-fish/oh-my-fish/raw/master/bin/install | fish
