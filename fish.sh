6da2705833e5306be946239d33df8dbff8416926
Aron Strandberg <aron@aronstrandberg.com>
2022-08-08 15:36:53 +0200

    set new homebrew path for fish

--- fish.sh
+++ fish.sh
@@ -1,9 +1,9 @@
 # set fish as default shell
-if [ $SHELL != "/usr/local/bin/fish" ]; then
-  if ! fgrep -q '/usr/local/bin/fish' /etc/shells; then
-    echo '/usr/local/bin/fish' | sudo tee -a /etc/shells
+if [ $SHELL != "/opt/homebrew/bin/fish" ]; then
+  if ! fgrep -q '/opt/homebrew/bin/fish' /etc/shells; then
+    echo '/opt/homebrew/bin/fish' | sudo tee -a /etc/shells
   fi
-  chsh -s /usr/local/bin/fish
+  chsh -s /opt/homebrew/bin/fish
 fi
 
 mkdir -p ~/.config
